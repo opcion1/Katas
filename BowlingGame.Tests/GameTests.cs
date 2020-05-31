2232e13fc4bb5626cb662de3ba2f7e222373ad36
opcion1 <tenken74@gmail.com>
2020-05-31 14:42:12 -0400

    Finish a first version
    use a validator and analyzor class and a calculator class.
    Test all three classes

---
+++ BowlingGame.Tests/GameTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BowlingGame.Tests
+{
+    public class GameTests
+    {
+        [Fact]
+        public void PerfectGame_Is300()
+        {
+            Game game = new Game();
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            int score = game.Score();
+
+            Assert.Equal(300, score);
+        }
+        [Fact]
+        public void Score_WhenSpare_CountNextDouble()
+        {
+            Game game = new Game();
+            game.Roll(6);
+            game.Roll(4);
+            int score1 = game.Score();
+            game.Roll(8);
+            int score2 = game.Score();
+            game.Roll(1);
+            int score3 = game.Score();
+
+            Assert.Equal(10, score1);
+            Assert.Equal(26, score2);
+            Assert.Equal(27, score3);
+        }
+        [Fact]
+        public void Score_WhenStrike_TwoNeoutDouble()
+        {
+            Game game = new Game();
+            game.Roll(6);
+            game.Roll(3);
+            int score1 = game.Score();
+            game.Roll(10);
+            int score2 = game.Score();
+            game.Roll(4);
+            int score3 = game.Score();
+            game.Roll(4);
+            int score4 = game.Score();
+
+            Assert.Equal(9, score1);
+            Assert.Equal(19, score2);
+            Assert.Equal(27, score3);
+            Assert.Equal(35, score4);
+        }
+    }
+}
---
+++ BowlingGame/Game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        Frame[] _emptyGame;
+        private readonly GameValidator _gameValidator;
+        private readonly GameScoreCalculator _gameScoreCalculator;
+
+        public Game()
+        {
+            _emptyGame = InitializeEmptyGame();
+            _gameValidator = new GameValidator(_emptyGame);
+            _gameScoreCalculator = new GameScoreCalculator();
+        }
+
+        private Frame[] InitializeEmptyGame()
+        {
+            Frame[] frames = new Frame[10];
+            for (int i = 0; i < 9; i++)
+            {
+                frames[i] = new Frame();
+            }
+            frames[9] = new Frame { IsLastOne = true };
+            return frames;
+        }
+
+        public void Roll(int pins)
+        {
+            (bool isValid, bool isStrike, bool isSpare) validAndAnalyse = _gameValidator.ValidateGame(pins);
+            if (validAndAnalyse.isValid)
+            {
+                _gameScoreCalculator.UpdateScore(pins, validAndAnalyse.isSpare, validAndAnalyse.isStrike);
+            }
+        }
+        public int Score()
+        {
+            return _gameScoreCalculator.Score;
+        }
+    }
+}
