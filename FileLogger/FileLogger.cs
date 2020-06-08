736e8f9d8d1b7285258f194c8cf14c78da83c72f
opcion1 <tenken74@gmail.com>
2020-06-08 17:09:23 -0400

    Create start Project and test project

---
+++ FileLogger/FileLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileLogger
+{
+    public static class FileLogger
+    {
+        public static void Log(string message)
+        {
+            using (StreamWriter sw = new StreamWriter("log.txt", true))
+            {
+                sw.WriteLine(message);
+                sw.Close();
+            }
+        }
+    }
+}
