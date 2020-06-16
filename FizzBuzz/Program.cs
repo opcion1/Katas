916e609f1a1c442d1b0de5c8209f192946835fb1
opcion1 <tenken74@gmail.com>
2020-06-16 10:06:57 -0400

    Add fizzbuzz kata
    This kata was added directly to the master branch because I had no connection a this time :(

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz();
+            Console.ReadLine();
+        }
+
+        public static void FizzBuzz()
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                Console.WriteLine(NumToFizzBuzz(i));
+            }
+        }
+
+        public static string NumToFizzBuzz(int num)
+        {
+            bool isMod3 = num % 3 == 0;
+            bool isMod5 = num % 5 == 0;
+
+            return
+                isMod3 && isMod5
+                    ? "FizzBuzz"
+                    : isMod3
+                        ? "Fizz"
+                        : isMod5
+                            ? "Buzz"
+                            : num.ToString();
+        }
+    }
+}
